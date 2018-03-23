724bedae9a419ee9217615e6361ba489dfd54b93
kaiware007 <kaiware007@gmail.com>
2018-03-24 03:12:54 +0900

    first commit

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Vector2 rotationSpeed;
+    public bool reverse;
+    private Vector2 lastMousePosition;
+    private Vector2 newAngle = new Vector2(0, 0);
+
+    void Update()
+    {
+        float dt = Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            newAngle = mainCamera.transform.localEulerAngles;
+            lastMousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (!reverse)
+            {
+                newAngle.y -= (lastMousePosition.x - Input.mousePosition.x) * rotationSpeed.y * dt;
+                newAngle.x -= (Input.mousePosition.y - lastMousePosition.y) * rotationSpeed.x * dt;
+                mainCamera.transform.localEulerAngles = newAngle;
+                lastMousePosition = Input.mousePosition;
+            }
+            else if (reverse)
+            {
+                newAngle.y -= (Input.mousePosition.x - lastMousePosition.x) * rotationSpeed.y * dt;
+                newAngle.x -= (lastMousePosition.y - Input.mousePosition.y) * rotationSpeed.x * dt;
+                mainCamera.transform.localEulerAngles = newAngle;
+                lastMousePosition = Input.mousePosition;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/THETAVCamTextureSetter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THETAVCamTextureSetter : MonoBehaviour {
+
+    #region define
+    static readonly string theta_v_FullHD = "RICOH THETA V FullHD";
+    static readonly string theta_v_4K = "RICOH THETA V 4K";
+
+    static readonly string[] thetaCameraModeList =
+    {
+        theta_v_FullHD,
+        theta_v_4K,
+    };
+
+    public enum THETA_V_CAMERA_MODE
+    {
+        THETA_V_FullHD,
+        THETA_V_4K,
+    }
+    #endregion
+
+    public THETA_V_CAMERA_MODE cameraMode = THETA_V_CAMERA_MODE.THETA_V_FullHD;
+
+    // Use this for initialization
+    void Start () {
+
+        int cameraIndex = -1;
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        Debug.Log("DevicesLength:" + devices.Length.ToString());
+        for (var i = 0; i < devices.Length; i++)
+        {
+            for (int j = 0; j < thetaCameraModeList.Length; j++)
+            {
+                if (devices[i].name == thetaCameraModeList[j])
+                {
+                    Debug.Log("[" + i + "] " + devices[i].name + " detected");
+                    cameraIndex = i;
+                    break;
+                }
+            }
+            if (cameraIndex >= 0) break;
+        }
+
+        if(cameraIndex < 0)
+        {
+            Debug.LogError("THETA V Not found");
+            return;
+        }
+
+        WebCamTexture webCamTexture = new WebCamTexture(devices[cameraIndex].name);
+        GetComponent<Renderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+	
+}
